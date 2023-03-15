1e9071f196e89132c773f2fc88cd3c4e2c2c261b
fortune <fortuneojih@yahoo.com>
2023-03-15 10:49:16 +0100

    created an implementation for currency conversion

--- currency-conversion-microservices/src/main/java/com/example/currencyconversionmicroservices/CurrencyConversionBean.java
+++ currency-conversion-microservices/src/main/java/com/example/currencyconversionmicroservices/CurrencyConversionBean.java
@@ -1,12 +1,11 @@
 package com.example.currencyconversionmicroservices;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
-@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyConversionBean {
     private Long id;
     private String from;
--- currency-conversion-microservices/src/main/java/com/example/currencyconversionmicroservices/CurrencyConversionService.java
+++ currency-conversion-microservices/src/main/java/com/example/currencyconversionmicroservices/CurrencyConversionService.java
@@ -1,15 +1,31 @@
 package com.example.currencyconversionmicroservices;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class CurrencyConversionService {
+    private final RestTemplate restTemplate;
+    Integer totalCalculatedAmount;
 
     public CurrencyConversionBean convertCurrency(String fromCurrency, String toCurrency, Integer quantity){
 
-       return new CurrencyConversionBean();
+        Map<String, String> uriVariables = new HashMap<>();
+
+        uriVariables.put("fromCurrency",fromCurrency);
+        uriVariables.put("toCurrency", toCurrency);
+
+        CurrencyConversionBean response = restTemplate.getForObject("https://localhost:8080/api/v1/currency-exchange/fromCurrency/{fromCurrency}/toCurrency/{toCurrency}",
+                CurrencyConversionBean.class, uriVariables);
+
+        totalCalculatedAmount = quantity * response.getConversionMultiple();
+        return new CurrencyConversionBean(response.getId(),fromCurrency,toCurrency,response.getConversionMultiple(), quantity,totalCalculatedAmount,response.getPort());
     }
 }
 
